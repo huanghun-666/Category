21e5afa703ad0894b8a331c964c4f8f22f621c5f
姚立飞 <huanghun-666@163.com>
2018-01-13 16:36:13 +0800

    no message

---
+++ UIImage+YLF.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+YLF.h
+//
+//  Created by 姚立飞 on 2017/8/31.
+//  Copyright © 2017年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (YLF)
+
+- (UIImage *)cutCircleImage;
+
+@end
---
+++ UIImage+YLF.m
@@ -0,0 +1,29 @@
+//
+//  UIImage+YLF.m
+//
+//  Created by 姚立飞 on 2017/8/31.
+//  Copyright © 2017年 KK. All rights reserved.
+//
+
+#import "UIImage+FF.h"
+
+@implementation UIImage (FF)
+
+- (UIImage *)cutCircleImage
+{
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    // 获取上下文
+    CGContextRef ctr = UIGraphicsGetCurrentContext();
+    // 设置圆形
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextAddEllipseInRect(ctr, rect);
+    // 裁剪
+    CGContextClip(ctr);
+    // 将图片画上去
+    [self drawInRect:rect];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+@end
